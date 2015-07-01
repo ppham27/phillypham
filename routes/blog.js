ffaf93543be4a641b149c11eb4312c3fd27622a9
Philip Pham <phil@phillypham.com>
2015-07-01 13:46:16 -0400

    some refactoring and style

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('post/create', {title: 'Post'});
+  res.render('post/index');
 });
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,14 @@ var router = express.Router();
 
 var db = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: db.ApplicationSettings.title });
-});
 
+router.use(require('./blog'));
 router.use('/login', require('./login'));
 router.use('/register', require('./register'));
 router.use('/logout', require('./logout'));
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
-
+router.use('/blog', require('./blog'));
 router.use('/post', require('./post'));
 
 module.exports = router;
